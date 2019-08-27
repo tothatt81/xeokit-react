b69dd3eb047f95c0c165bd46d00acd8addd07f9e
Barnabás Molnár <barna.molnar90@gmail.com>
2019-08-27 20:50:55 +0200

    src

--- demo/src/navcube_settings.js
+++
@@ -1,9 +0,0 @@
-export const sampleNavCubeSettings = {
-    canvasId: "myNavCubeCanvas",
-    visible: true,
-    cameraFly: true,
-    cameraFitFOV: 45,
-    cameraFlyDuration: 0.5,
-    canvasWidth: 250,
-    canvasHeight: 250
-};
